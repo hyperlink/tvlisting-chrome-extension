c07fc205efeadca3bc66eb0715c6180bad43019e
Xiaoxin Lu <javascript@yahoo.com>
2013-12-04 22:05:12 -0500

    Fix issue with channel Line-ups in the 28304 or 28306 Zip codes

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -2,6 +2,7 @@
 
 var $zipCode = $('#zipCode').val(localStorage.getItem('zip'))
 var BASE     = 'http://tvlistings.timewarnercable.com'
+var lastZip  = ''
 
 $(".alert").alert()
 $('.error, #clearCache').hide()
@@ -11,14 +12,10 @@ var $searchBtn = $('#searchBtn').click(function(){
 	if ( zipVal ) {
 		var searchBtn = $(this).button('loading')
 		$.get(BASE +'/findTvListings/?zipCode='+ zipVal).done(function(data){
-			var elements = parseLineups(data)
-			if (elements.length) {
-				localStorage.clear()
-				localStorage.setItem('lineup', BASE + elements[0].getAttribute('href'))
-				searchBtn.button('complete').attr('disable', true)
-			} else {
-				onFailure('No TWC channel lineups for this area.')
-				searchBtn.button('reset')
+			lastZip = zipVal
+			var lineup = extractChannelListUrl(parseLineups(data))
+			if (lineup) {
+				storeLineup(lineup)
 			}
 			updateCacheBtn()
 		}).fail(onFailure)
@@ -26,6 +23,17 @@ var $searchBtn = $('#searchBtn').click(function(){
 		$zipCode.focus()
 	}
 })
+
+function storeLineup(lineup) {
+	localStorage.clear()
+	localStorage.setItem('lineup', lineup)
+	$searchBtn.button('complete').attr('disable', true)
+}
+
+function noLineupFound() {
+	onFailure('No TWC channel lineups for this area.')
+	$searchBtn.button('reset')
+}
 
 var $clearCacheBtn = $('#clearCache')
 
@@ -35,6 +43,15 @@ $clearCacheBtn.click(function(){
 	localStorage.removeItem('cache')
 	updateCacheBtn()
 })
+
+window.addEventListener('message', function(event) {
+	if (event.data.successful) {
+		var lineup = [BASE,'/findTvListings/viewChannelLineUp/?headendId=', event.data.headEndId, '&zipCode=', lastZip].join('')
+		storeLineup(lineup)
+	} else {
+		noLineupFound()
+	}
+});
 
 function updateCacheBtn() {
 	if (localStorage.getItem('cache')) {
@@ -47,7 +64,26 @@ function updateCacheBtn() {
 function parseLineups(data) {
 	var template = document.createElement('template')
 	template.innerHTML = data
-	return template.content.querySelectorAll('a.arrowBtn:first-child')
+	return template.content
+}
+
+function extractChannelListUrl(templateContent) {
+	var elements = templateContent.querySelectorAll('a.arrowBtn:first-child')
+	if (elements.length) {
+		return BASE + elements[0].getAttribute('href')
+	}
+	parseJavaScript(templateContent.querySelectorAll('script:not([src])')[0].innerHTML)
+	return ''
+}
+
+function parseJavaScript(source) {
+	console.log(source)
+
+	 var iframe = document.getElementById('theFrame')
+	 var message = {
+	 	source: source
+	 }
+	 iframe.contentWindow.postMessage(message, '*')
 }
 
 function onFailure (data) {
